deb26266110d21e48c7a611659b4a69a5bf1b735
Radhakrishnan <raokikrishnan@gmail.com>
2022-12-25 00:00:07 +0530

    Added the changes for clippy and also state variable change

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
 #![allow(dead_code, unused_variables)]
-
+use std::io;
 fn main() {
     println!("Hello lets play ordinary tic tac toe");
-    for i in 0..3 {
-        println!("{}", i)
+    let mut my_board = TicTacToeBoard::new();
+    my_board.show();
+    for turn in 0..9 {
+        println!("Enter your move (row , column)");
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).unwrap();
+        let numbers: Vec<u8> = user_input
+            .split_ascii_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+        let (x, y) = (*numbers.first().unwrap(), *numbers.get(1).unwrap());
+        my_board = my_board.get_move((x, y));
+        my_board.show();
     }
-    let my_board = TicTacToeBoard::new();
-    my_board.show()
 }
 
 struct TicTacToeBoard {
     x_value: u16,
     o_value: u16,
+    x_is_player: bool,
 }
 impl TicTacToeBoard {
     fn new() -> TicTacToeBoard {
         TicTacToeBoard {
             x_value: 0,
             o_value: 0,
+            x_is_player: true,
         }
     }
     fn show(&self) {
+        println!(
+            "{} to play",
+            match self.x_is_player {
+                true => "X",
+                false => "O",
+            }
+        );
         println!(" _ _ _ ");
         for i in 0..3 {
             let mut line_vec = Vec::new();
             for j in 0..3 {
-                if self.x_value >> (3 * i + j) == 1 {
+                if (self.x_value >> (3 * i + j)) & 1 == 1 {
                     line_vec.push("X")
-                } else if self.o_value >> (3 * i + j) == 1 {
+                } else if (self.o_value >> (3 * i + j)) & 1 == 1 {
                     line_vec.push("O")
                 } else {
                     line_vec.push(" ")
@@ -36,6 +54,26 @@ impl TicTacToeBoard {
             println!("|{}|{}|{}|", line_vec[0], line_vec[1], line_vec[2]);
         }
         println!(" ‾ ‾ ‾ ")
+    }
+    fn get_move(&self, action: (u8, u8)) -> TicTacToeBoard {
+        assert!((action.0 < 3) & (action.1 < 3));
+        let action_point = 3 * action.0 + action.1;
+        let shift = 1 << action_point;
+        assert!((self.x_value >> action_point) & 1 == 0);
+        assert!((self.o_value >> action_point) & 1 == 0);
+        if self.x_is_player {
+            TicTacToeBoard {
+                x_value: self.x_value + shift,
+                o_value: self.o_value,
+                x_is_player: !self.x_is_player,
+            }
+        } else {
+            TicTacToeBoard {
+                x_value: self.x_value,
+                o_value: self.o_value + shift,
+                x_is_player: !self.x_is_player,
+            }
+        }
     }
 }
 mod time_module {
@@ -73,13 +111,13 @@ mod time_module {
         let ten = 10u128;
         let elapsed_float = elapsed as f64;
         if elapsed > ten.pow(9) {
-            return format!("{:.3} s", elapsed_float / (ten.pow(9) as f64));
+            format!("{:.3} s", elapsed_float / (ten.pow(9) as f64))
         } else if elapsed > ten.pow(6) {
-            return format!("{:.3} ms", elapsed_float / (ten.pow(6) as f64));
+            format!("{:.3} ms", elapsed_float / (ten.pow(6) as f64))
         } else if elapsed > ten.pow(3) {
-            return format!("{:.3} µs", elapsed_float / (ten.pow(3) as f64));
+            format!("{:.3} µs", elapsed_float / (ten.pow(3) as f64))
         } else {
-            return format!("{:.3} ns", elapsed_float);
+            format!("{:.3} ns", elapsed_float)
         }
     }
 }
